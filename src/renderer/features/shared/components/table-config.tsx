45096dc723d4ea70158e7bcbb981ac3c770ff81c
Rum <40669776+rumzie@users.noreply.github.com>
2026-09-06 22:34:29 -0400

    configure settings modal
    on the now playing screen, on the configure settings modal, the table columns settings are not visible and go off screen for the mobile view

--- src/renderer/features/shared/components/table-config.tsx
+++ src/renderer/features/shared/components/table-config.tsx
@@ -774,7 +774,7 @@ const TableColumnItem = memo(
                 })}
                 ref={ref}
             >
-                <Group wrap="nowrap">
+                <Group className={styles.columnLabel} wrap="nowrap">
                     <DragHandle dragHandleRef={dragHandleRef} />
                     <Checkbox
                         checked={item.isEnabled}
@@ -784,7 +784,7 @@ const TableColumnItem = memo(
                         size="sm"
                     />
                 </Group>
-                <Group wrap="nowrap">
+                <Group className={styles.columnControls} wrap="nowrap">
                     <ActionIconGroup className={styles.group}>
                         <ActionIcon
                             icon="arrowUp"
